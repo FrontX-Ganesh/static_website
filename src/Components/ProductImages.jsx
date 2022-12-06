a9c6867c6446609a55fb8a91d66a401ad5b451f3
GaneshProject <104259891+FrontX-Ganesh@users.noreply.github.com>
2022-12-06 22:22:01 +0530

    images and info

--- src/Components/ProductImages.jsx
+++ src/Components/ProductImages.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
-const ProductImages = ({ image }) => {
-    if(image == undefined){
-        return <div> Product Images Loading</div>
-    }
+const ProductImages = ({ image  = [] }) => {
+    
     return (
         <>  
-            <div>
-            {   
-                image.map((curEle, index) => {
-                    return <img src={curEle.url} key={index} alt="hello" height={170} width={170}></img>
-                })
-            }
+            <div className='decoration'>
+                {   
+                    image.map((curEle, index) => {
+                        return <img src={curEle.url} key={index} alt="hello" height={170} width={170}></img>
+                    })
+                }
             </div>
         </>
         
--- src/Components/SingleProduct.js
+++ src/Components/SingleProduct.js
@@ -1,8 +1,10 @@
 import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { AppContext } from '../Context/ProductContext';
 import FormatPrice from '../Helper/FormatPrice';
 import ProductImages from './ProductImages';
+import Stars from './Stars';
 import '../Css/SingleProduct.css';
 
 const API = "https://api.pujakaitem.com/api/products";
@@ -34,18 +36,21 @@ const SingleProduct = () => {
 
   return (
     <>
+      <div>
+        <Link to='/' style={{textDecoration: "none"}}>Home/</Link>{ name }
+      </div>
       <div className='productData'> 
         <div className='productImages'>
           <ProductImages image={image}/>
         </div>
         <div className='productDetails' style={ {width:'40%'} }>
           <h1>{ name }</h1>
-          <div>{ stars }</div>
+          <div><Stars stars={stars} reviews={reviews} /></div>
           <div>{ reviews } { (reviews <= 1) ? 'Review' : 'Reviews' }</div>
           <div>MRP :- <del> <FormatPrice price = {price + 2500}/></del></div>
-          <div> <span className='dealOfDay'><b>Deal Of the Day</b> </span> :-  <FormatPrice price = {price}/> </div>
+          <div> <span className='dealOfDay'><b>Deal Of the Day</b> </span> :- <FormatPrice price = {price}/> </div>
           <div>{ description }</div>
-          <div style={{display : 'flex', justifyContent: 'space-between'}}><p> Fast Delivery </p> <p> 30 Days Replacemnt  </p> <p> Ganesh Delivered </p> <p> 1 Year Warrenty </p></div>
+          <div style={{display : 'flex', justifyContent: 'space-between'}}><p> Fast Delivery </p> <p> 30 Days Replacemnt </p> <p> Ganesh Delivered </p> <p> 1 Year Warrenty </p></div>
           <div> <p> Available : <span> {stock > 0 ? 'In Stock' : 'Not Available'} </span> </p></div>
           <div> ID :- <span> { id } </span></div>
           <div> Brand :- <span> { company } </span></div>
